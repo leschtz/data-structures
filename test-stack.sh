8449f573901e8aad7595cd7d6d3b5f19d062f031
Lukas Schütz <schuetz.lukas@sbg.at>
2016-11-22 09:11:38 +0100

    syntax formatting

--- test-stack.sh
+++ test-stack.sh
@@ -5,12 +5,11 @@ if [ -f stack ]
     rm stack
 fi
 
-if [ -f stack.c ]
-  then
-    gcc -Wall -o stack stack.c
-  else
-    echo "File 'stack.c' not found."
-    exit -1
+if [ -f stack.c ]; then
+  gcc -Wall -o stack stack.c
+else
+  echo "File 'stack.c' not found."
+  exit -1
 fi
 
 ./stack > stack.txt
@@ -21,11 +20,10 @@ POP=$(tac stack.txt | grep 'POP:' | sed 's/[^0-9]//g')
 printf "%4d\n" ${POP} > POP
 
 DIFF=$(diff PUSH POP)
-if [ "$DIFF" != "" ]
-  then
-    echo "The operations failed."
-    exit -2
-  else
-    echo "The stack did work correctly."
-    exit 0
+if [ "$DIFF" != "" ]; then
+  echo "The operations failed."
+  exit -2
+else
+  echo "The stack did work correctly."
+  exit 0
 fi
